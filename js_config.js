b61fec856feedd8206a48aeb94e61306ae9233c6
kitaq_itclub <resuscitation.project@gmail.com>
2026-01-14 00:51:53 +0900

    Update js_config.js

--- js_config.js
+++ js_config.js
@@ -1,17 +1,16 @@
 // Google Forms 設定
-// 以下のIDは実際のGoogle FormsのIDに置き換える
 const CONFIG = {
-  // Google FormsのURL (後で設定)
-  FORM_URL: 'https://docs.google.com/forms/d/e/YOUR_FORM_ID/formResponse',
+  // Google FormsのURL
+  FORM_URL: 'https://docs.google.com/forms/d/e/1FAIpQLScjedxPV9AIiIJGXDLw-Ovx2MHL6J2j5dIJnOegrqyxLyWzOw/formResponse',
   
-  // 各項目のentry ID (後で設定)
+  // 各項目のentry ID
   ENTRY_IDS: {
-    type: 'entry.123456789',        // 参加区分（初回/当日）
-    name: 'entry.234567890',        // お名前
-    category: 'entry.345678901',    // 区分（学生/社会人）
-    price: 'entry.456789012',       // 料金
-    contact: 'entry.567890123',     // 連絡先
-    memo: 'entry.678901234',        // 備考
-    timestamp: 'entry.789012345'    // タイムスタンプ
+    type: 'entry.545390933',      // 参加区分
+    name: 'entry.1112925744',     // お名前
+    category: 'entry.752439379',  // 区分
+    price: 'entry.783987414',     // 料金
+    contact: 'entry.92546563',    // 連絡先
+    memo: 'entry.1301860409'      // 備考
   }
 };
+```
