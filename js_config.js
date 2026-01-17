aca1fab055d0230958f7c8f7bb365763cd22ad7a
kitaq_itclub <resuscitation.project@gmail.com>
2026-01-17 23:37:00 +0900

    Update js_config.js

--- js_config.js
+++ js_config.js
@@ -1,15 +1,5 @@
 // Google Forms 設定
 const CONFIG = {
-  // Google FormsのURL
-  FORM_URL: 'https://docs.google.com/forms/d/e/1FAIpQLScjedxPV9AIiIJGXDLw-Ovx2MHL6J2j5dIJnOegrqyxLyWzOw/formResponse',
-  
-  // 各項目のentry ID
-  ENTRY_IDS: {
-    type: 'entry.545390933',      // 参加区分
-    name: 'entry.1112925744',     // お名前
-    category: 'entry.752439379',  // 区分
-    price: 'entry.783987784',     // 料金
-    contact: 'entry.92546563',    // 連絡先
-    memo: 'entry.1301860409'      // 備考
-  }
+  // GAS Web AppのURL
+  FORM_URL: 'https://script.google.com/macros/s/AKfycbyBP8yKennsi7Sr5fR-fnKJnypSE6E4EUpecKGjwFu_2D1GixDBP4qi2VOsZi8viGv0Eg/exec',
 };
