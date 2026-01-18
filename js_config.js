2efc44d67f400ccc1dc47567b8f528cd201ad6e3
kitaq_itclub <resuscitation.project@gmail.com>
2026-01-18 22:54:36 +0900

    Update js_config.js

--- js_config.js
+++ js_config.js
@@ -1,5 +1,5 @@
 // Google Apps Script Web App設定
 const CONFIG = {
-  // GAS Web AppのURL（スプレッドシート直接書き込み版）
-  FORM_URL: 'https://script.google.com/macros/s/AKfycbwimqCtkO4KyfQoQEx2JxRC46iw58Mis10DqgWarAMH0d0qmCth-TTm7kLUhRiFgmU2BQ/exec',
+  // GAS Web AppのURL（スプレッドシート直接書き込み版 v2）
+  FORM_URL: 'https://script.google.com/macros/s/AKfycbzVVDJsfK2wTWlK-CxOVvqYNgHGlYFzyytPqpM_DJou5rI79eSGtDDkGRU-MOPGsBzc3Q/exec',
 };
